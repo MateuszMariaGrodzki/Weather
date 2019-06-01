dc0463524affa3704aa736b2a3a1308daffa2556
Mateusz <smg7@o2.pl>
2019-06-01 13:52:52 +0200

    added form

--- src/main/java/com/example/demo/api/WeatherController.java
+++ src/main/java/com/example/demo/api/WeatherController.java
@@ -2,6 +2,7 @@ package com.example.demo.api;
 
 import com.example.demo.model.WeatherModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,7 +13,7 @@ public class WeatherController {
 
     @GetMapping("/weather")
     @ResponseBody
-    public String getWeather(@RequestParam String city){
+    public String getWeather(@RequestParam String city, ModelMap modelMap){
         String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",pl&appid=ef2028e98b54649bf1f4c4582631f350";
         WeatherModel weatherModel = new RestTemplate().getForObject(url, WeatherModel.class);
         return weatherModel.toString();
